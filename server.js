258eb5fc67e5eeb79da9b5e9b7a938557eaed97c
europaeden <s.stratton@rgu.ac.uk>
2025-04-13 23:04:30 +0100

    Cmmt

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
+// Serve static files from the 'pages' directory
+app.use(express.static(path.join(__dirname, 'pages')));
+// Serve static files from root directory (for styles.css)
+app.use(express.static(__dirname));
+
+// Route for the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'Home.html'));
+});
+
+// Webhook endpoint
 app.post("/webhook", (req, res) => {
     console.log("Webhook request received:", req.body);
     
